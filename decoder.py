2d074e581b2e25a6ba066a21f68e546b3c7053cb
Joaquin Peraza <joaquin@peraza.uy>
2023-12-09 07:19:25 -0600

    misc

---
+++ decoder.py
@@ -0,0 +1,12 @@
+import base64
+
+
+def main():
+    a='zKuEq5Xy6OOEy8zmslKIn5TXnNvQ'
+    b=b"zKuEq5Xy6OOEy8zmslKIn5TXnNvQ"
+    r=(base64.decodebytes(b))
+    print([str. for b in r])
+
+
+if __name__ == '__main__':
+    main()
